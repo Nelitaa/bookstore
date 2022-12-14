3fafa4c8a877d80d61efca43e69fff05205b9f02
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-14 10:59:31 -0400

    Add functionality for Remove a book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeBookAction } from '../redux/books/books';
 
 const Book = (props) => {
   const { id, title, author } = props;
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(removeBookAction(id));
+  };
+
   return (
     <li key={id}>
       <h3>{title}</h3>
       <p>{author}</p>
-      <button type="button">Remove</button>
+      <button type="button" onClick={handleClick}>Remove</button>
     </li>
   );
 };
