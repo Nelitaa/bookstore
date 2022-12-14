04b849b12ca919fdea56a209de0aee7b4aaa9345
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-14 11:42:28 -0400

    Add functionality for Check status button

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -7,15 +7,15 @@ const initialState = {
 const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHECK_STATUS:
-      return 'Under Construction';
+      return window.alert(action.payload);
     default:
       return state;
   }
 };
 
-const checkStatusAction = (category) => ({
+const checkStatusAction = () => ({
   type: CHECK_STATUS,
-  payload: category,
+  payload: 'Under construction',
 });
 
 export { checkStatusAction };
